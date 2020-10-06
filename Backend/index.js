a3faac8ebbe8fbfba9437f83ccb7c2aaeb5a44de
trishten <42450374+trishten@users.noreply.github.com>
2020-10-05 21:44:39 -0500

    Backend working ...

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,13 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const PORT = 4000;
-app.use(cors());
-app.listen(PORT, function () {
-  console.log("Server is running on Port: " + PORT);
-});
+const mongoose = require("mongoose");
+// const PORT = 4000;
+// app.use(cors());
+const uri =
+  "mongodb+srv://user:user1@urlshortner.qbreh.mongodb.net/urlshort?retryWrites=true&w=majority";
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+const Person = mongoose.model("Person", { name: String });
+
+const person = new Person({ name: "Trey" });
+person.save().then(() => console.log("hit"));
+// app.listen(PORT, function () {
+//   console.log("Server is running on Port: " + PORT);
+// });
